4b1cc81dc392a5c8b81dc1b23cb294540c45a9f9
El-Arif AHAMADA <elarif.ahamada@gmail.com>
2015-12-02 22:11:22 +0100

    reactive 2

--- src/main/java/rubik22/reactor/TestReactor.java
+++ src/main/java/rubik22/reactor/TestReactor.java
@@ -3,18 +3,19 @@ package rubik22.reactor;
 import java.text.MessageFormat;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.reactivestreams.Processor;
 
 import reactor.Environment;
-import reactor.core.Dispatcher;
-import reactor.fn.Consumer;
+import reactor.core.processor.RingBufferWorkProcessor;
+import reactor.rx.Stream;
+import reactor.rx.Streams;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.Pipeline;
 import redis.clients.jedis.Response;
-import redis.clients.jedis.ScanParams;
-import redis.clients.jedis.ScanResult;
 import rubik22.generator.RubikRotationImage;
 import rubik22.model.AbstractRubik;
 import rubik22.model.Rotation;
@@ -24,68 +25,64 @@ import rubik3.model.Rubik;
 import com.google.common.collect.ImmutableList;
 
 public class TestReactor {
-  static {
-    // Only done once, statically, and shared across this classloader
-    Environment.initialize();
-  }
+	static {
+		// Only done once, statically, and shared across this classloader
+		Environment.initialize();
+	}
 
-  private final static String IP_REDIS = "192.168.1.67";
+	private final static String IP_REDIS = "192.168.1.67";
 
-  public static void main(String[] args) throws InterruptedException {
-    Dispatcher dispatcher = Environment.sharedDispatcher();
-    Consumer<Throwable> errorHandler = t -> {
-      t.printStackTrace();
-    };
-    Consumer<AbstractRubik> c = new Consumer<AbstractRubik>() {
-      Jedis j = new Jedis(IP_REDIS, 6379);
+	public static void main(String[] args) throws InterruptedException {
+		JedisPoolConfig poolConfig = new JedisPoolConfig();
+		poolConfig.setMaxTotal(5);
+		JedisPool jedisPool = new JedisPool(poolConfig, IP_REDIS);
+		Jedis j = jedisPool.getResource();
+		Jedis k = jedisPool.getResource();
+		Processor<String, String> p = RingBufferWorkProcessor.create();
+		Stream<String> s = Streams.wrap(p);
 
-      @Override
-      public void accept(AbstractRubik rubik) {
-        List<RubikRotationImage> images = Collections.emptyList();
-        ImmutableList.Builder<RubikRotationImage> builder = ImmutableList.builder();
-        for (Rotation rotation : Rotation.values()) {
-          builder.add(new RubikRotationImage.Builder().withRubik(rubik).withRotation(rotation)
-              .withImage(rubik.rotate(rotation)).build());
-        }
-        images = builder.build();
-        String hashcode = String.valueOf(rubik.hashCode());
-        ImmutableList.Builder<Response<Boolean>> exists = ImmutableList.builder();
-        Pipeline p = j.pipelined();
-        p.set(hashcode, rubik.toString());
-        for (String string : rubik.aliases()) {
-        	p.set(String.valueOf(string.hashCode()), string);
-        }
-        for (RubikRotationImage rri : images) {
-          String key = MessageFormat.format("{0},{1}", rri.rubik.toString(), rri.rotation.name());
-          String value = rri.image.toString();
-          String imageHash = String.valueOf(rri.image.hashCode());
-          p.set(key, value);
-          exists.add(p.exists(imageHash));
-        }
-        p.sync();
-        List<Response<Boolean>> responses = exists.build();
-        for (int i = 0; i < responses.size(); i++) {
-          if(!responses.get(i).get()){
-            j.sadd("TODO", images.get(i).image.toString());
-          }
-        }
-      }
-    };
+		Processor<AbstractRubik, AbstractRubik> q = RingBufferWorkProcessor
+				.create();
+		Stream<AbstractRubik> t = Streams.wrap(q);
+		s.map(Rubik::valueOf).consume(c -> {
+			 List<RubikRotationImage> images = Collections.emptyList();
+		        ImmutableList.Builder<RubikRotationImage> builder = ImmutableList.builder();
+		        for (Rotation rotation : Rotation.values()) {
+		          builder.add(new RubikRotationImage.Builder().withRubik(c).withRotation(rotation)
+		              .withImage(c.rotate(rotation)).build());
+		        }
+		        images = builder.build();
+		        String hashcode = String.valueOf(c.hashCode());
+		        ImmutableList.Builder<Response<Boolean>> exists = ImmutableList.builder();
+		        Pipeline pipelined = j.pipelined();
+		        pipelined.set(hashcode, c.toString());
+		        for (String string : c.aliases()) {
+		        	pipelined.set(String.valueOf(string.hashCode()), string);
+		        }
+		        for (RubikRotationImage rri : images) {
+		          String key = MessageFormat.format("{0},{1}", rri.rubik.toString(), rri.rotation.name());
+		          String value = rri.image.toString();
+		          String imageHash = String.valueOf(rri.image.hashCode());
+		          pipelined.set(key, value);
+		          exists.add(pipelined.exists(imageHash));
+		        }
+		        pipelined.sync();
+		        List<Response<Boolean>> responses = exists.build();
+		        for (int i = 0; i < responses.size(); i++) {
+		          if(!responses.get(i).get()){
+		            j.sadd("TODO", images.get(i).image.toString());
+		          }
+		        }
+		});
+		k.sadd("TODO", new Rubik.Builder().withCubies(Cubie.values()).build().toString());
+		Long scard = k.scard("TODO");
+		while (scard > 0) {
+			String spop = k.spop("TODO");
+			p.onNext(spop);
+			scard = k.scard("TODO");
+		}
+		k.close();
+		j.close();
+	}
 
-    Jedis jedis = new Jedis(IP_REDIS, 6379, 1000 * 1000, 1000 * 1000);
-    jedis.configSet("timeout", "10");
-    jedis.sadd("TODO", new Rubik.Builder().withCubies(Cubie.values()).build().toString());
-    ScanResult<String> scanResult = jedis.sscan("TODO", "0", new ScanParams().count(1000));
-    long start = System.nanoTime();
-    while (!scanResult.getResult().isEmpty()) {
-      for (String s : scanResult.getResult()) {
-        dispatcher.dispatch(Rubik.valueOf(s), c, errorHandler);
-      }
-      scanResult = jedis.sscan("TODO", scanResult.getStringCursor(),
-          new ScanParams().count(100000));
-    }
-    System.out.println((System.nanoTime() - start) / 1000 + " µs");
-    jedis.close();
-    Environment.terminate();
-  }
 }
