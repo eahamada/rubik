c345488b516595911fd01a3837767208c7a65425
Elarif AHAMADA <mohamed.ahamada@cgi.com>
2015-12-18 18:33:42 +0100

    dfa minimisation

--- src/main/java/dfa/DFA.java
+++ src/main/java/dfa/DFA.java
@@ -1,21 +1,33 @@
 package dfa;
 
+import java.text.MessageFormat;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
 
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import com.google.common.collect.Sets.SetView;
 
 public class DFA {
 	public final Set<State> Q;
-	public final Set<Character> SIGMA;
-	public final Map<StateCharacter, State> delta;
+	public final Set<Character> Σ;
+	public final BiFunction<State, Character, State> δ;
 	public final State q0;
 	public final Set<State> F;
+	
+	public final static BiFunction<State, Character, String> PATTERN =
+      (state, character) -> MessageFormat.format("{0}:{1}", state, character);
 
 	private DFA(Builder builder) {
 		this.Q = builder.Q;
-		this.SIGMA = builder.SIGMA;
-		this.delta = builder.delta;
+		this.Σ = builder.Σ;
+		this.δ = builder.δ;
 		this.q0 = builder.q0;
 		this.F = builder.F;
 	}
@@ -23,8 +35,8 @@ public class DFA {
 	public static class Builder {
 
 		private Set<State> Q;
-		private Set<Character> SIGMA;
-		private Map<StateCharacter, State> delta;
+		private Set<Character> Σ;
+		private BiFunction<State, Character, State> δ;
 		private State q0;
 		private Set<State> F;
 
@@ -41,22 +53,22 @@ public class DFA {
 			return withQ(builder.build());
 		}
 
-		public Builder withSIGMA(Set<Character> SIGMA) {
-			this.SIGMA = SIGMA;
+		public Builder withΣ(Set<Character> Σ) {
+			this.Σ = Σ;
 			return this;
 		}
 
-		public Builder withSIGMA(char... SIGMA) {
+		public Builder withΣ(char... SIGMA) {
 			ImmutableSet.Builder<Character> builder = ImmutableSet.builder();
 			for (char c : SIGMA) {
 				builder.add(c);
 			}
-			return withSIGMA(builder.build());
+			return withΣ(builder.build());
 		}
 
-		public Builder withDelta(
-				Map<StateCharacter, State> delta) {
-			this.delta = delta;
+		public Builder withδ(
+		    BiFunction<State, Character, State> δ) {
+			this.δ = δ;
 			return this;
 		}
 
@@ -87,7 +99,7 @@ public class DFA {
 		boolean result = false;
 		State r = q0;
 		for (int i = 0; i < word.length() && r!=null; i++) {
-			r = delta.get(new StateCharacter(r,word.charAt(i)));
+			r = δ.apply(r,word.charAt(i));
 		}
 		result = F.contains(r);
 		return result;
@@ -97,7 +109,7 @@ public class DFA {
 		State t = q0;
 		int i = 0;
 		while (i < word.length() && t!=null) {
-			t = delta.get(new StateCharacter(r,word.charAt(i)));
+			t = δ.apply(r, word.charAt(i));
 			if(t != null){
 				r = t;
 				i++;
@@ -107,5 +119,98 @@ public class DFA {
 		System.out.println(substring);
 		return null;
 	}
+	
+	
+   @SuppressWarnings("unchecked")
+  public DFA removeUnreachableStates(){
+      Set<State> reachable_states = ImmutableSet.of(q0);
+      Set<State> new_states = Collections.EMPTY_SET;
+      do{
+        Set<State> temp = Collections.EMPTY_SET;
+        for (State q : new_states) {
+          for (Character c : Σ) {
+            temp = Sets.union(temp, ImmutableSet.of(δ.apply(q,c))).immutableCopy();
+          }
+        }
+        new_states = Sets.difference(temp, reachable_states).immutableCopy();
+        reachable_states = Sets.union(reachable_states, new_states).immutableCopy();
+      }while(!new_states.isEmpty());
+      return new DFA.Builder()
+        .withQ(reachable_states)
+        .withΣ(Σ)
+        .withδ(δ)
+        .withQ0(q0)
+        .withF(F)
+        .build();
+   }
+   @SuppressWarnings("unchecked")
+  public DFA minimize(){
+     Set<Set<State>> P = Sets.newHashSet(F, Sets.difference(Q, F).immutableCopy());
+     Set<Set<State>> W = Sets.newHashSet(F);
+     while(!W.isEmpty()){
+       Set<State> A = W.iterator().next();
+       W.remove(A);
+       for (Character c : Σ) {
+        Set<State> X = Q.stream().filter(s -> A.contains(δ.apply(s,c))).collect(Collectors.toSet());
+        
+        Set<Set<State>> Ys = P.stream().filter(Y -> !Sets.intersection(X, Y).isEmpty() && !Sets.difference(Y, X).isEmpty()).collect(Collectors.toSet());
+        for (Set<State> Y : Ys) {
+          Set<State> XInterY = Sets.intersection(X, Y);
+          SetView<State> YDiffX = Sets.difference(Y, X);
+          P.remove(Y);
+          P.add(XInterY);
+          P.add(YDiffX);
+          if(W.contains(Y)){
+            W.remove(Y);
+            W.add(XInterY);
+            W.add(YDiffX);
+          }else{
+            BiFunction<Set<State>,Set<State>,Set<State>> min = (s0, s1) -> s0.size() <= s1.size() ? s0:s1;
+            W.add(min.apply(XInterY,YDiffX));
+          }
+          
+        }
+      }
+     }
+     final AtomicInteger i = new AtomicInteger(0);
+    Set<State> minQ = P.stream().map(s -> new State.Builder().withId("q"+i.getAndIncrement()).build()).collect(Collectors.toSet());
+    State minQ0 = P.stream().filter(s -> s.contains(q0)).map(s -> new State.Builder().withId(String.valueOf(s.hashCode())).build()).collect(Collectors.toList()).get(0);
+    BiFunction<State, Character, State> minδ = (s,c) -> {
+      return new State.Builder().withId("").build();
+    };
+    Set<State> minF = P.stream().filter(s -> !Sets.intersection(s, F).isEmpty()).map(s -> new State.Builder().withId(String.valueOf(s.hashCode())).build()).collect(Collectors.toSet());
+    return new DFA.Builder()
+         .withQ(minQ)
+         .withQ0(minQ0)
+         .withδ(minδ)
+         .withF(minF)
+         .withΣ(Σ)
+         .build();
+   }
+   
+   public void prettyPrint(){
+     final PrettyPrinter printer = new PrettyPrinter(System.out);
+     String[][] arrays = new String[Q.size()+1][Σ.size()+2] ;
+     arrays[0][0] ="";
+     arrays[0][1] ="";
+     int j = 2;
+     for (Character c : Σ) {
+      arrays[0][j] = String.valueOf(c);
+      int i = 1;
+      for (State q : Q) {
+        arrays[i][0] ="";
+        if(q == q0){
+          arrays[i][0] += ">";
+        }
+        if(F.contains(q)){
+          arrays[i][0] += "*";
+        }
+          arrays[i][1] = q.id;
+          arrays[i++][j] = δ.apply(q, c).id;
+      } 
+      j++;
+    }
+    printer.print(arrays);
+   }
 
 }
--- src/test/java/dfa/DfaTest.java
+++ src/test/java/dfa/DfaTest.java
@@ -1,36 +1,117 @@
 package dfa;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static dfa.DFA.PATTERN;
 
 import java.util.Map;
+import java.util.function.BiFunction;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.google.common.collect.ImmutableMap;
+
 public class DfaTest {
 
-	@Test
+  @Test
+  @Ignore
 	public void test() {
-		State s1 = new State.Builder().withId("s1").build();
-		State s2 = new State.Builder().withId("s2").build();
-		
-		Map<StateCharacter, State> delta = ImmutableMap.of(
-											new StateCharacter(s1, '0'), s2,
-//											new StateCharacter(s1, '1'), s1,
-											new StateCharacter(s2, '0'), s1,
-											new StateCharacter(s2, '1'), s2											
-											);
+	  State q0 = new State.Builder().withId("q0").build();
+		State q1 = new State.Builder().withId("q1").build();
+		State q2 = new State.Builder().withId("q2").build();
+
+		BiFunction<State, Character, State> δ =
+		    (s, c) -> {
+		    ImmutableMap.Builder<String, State> builder = ImmutableMap.builder();
+		    Map<String, State> map = builder
+        .put(PATTERN.apply(q0,'0'), q0)
+        .put(PATTERN.apply(q0,'3'), q0)
+        .put(PATTERN.apply(q0,'6'), q0)
+        .put(PATTERN.apply(q0,'9'), q0)
+
+        .put(PATTERN.apply(q0,'1'), q1)
+        .put(PATTERN.apply(q0,'4'), q1)
+        .put(PATTERN.apply(q0,'7'), q1)
+
+        .put(PATTERN.apply(q0,'2'), q2)
+        .put(PATTERN.apply(q0,'5'), q2)
+        .put(PATTERN.apply(q0,'8'), q2)
+
+        .put(PATTERN.apply(q1,'0'), q1)
+        .put(PATTERN.apply(q1,'3'), q1)
+        .put(PATTERN.apply(q1,'6'), q1)
+        .put(PATTERN.apply(q1,'9'), q1)
+
+        .put(PATTERN.apply(q1,'1'), q2)
+        .put(PATTERN.apply(q1,'4'), q2)
+        .put(PATTERN.apply(q1,'7'), q2)
+
+        .put(PATTERN.apply(q1,'2'), q0)
+        .put(PATTERN.apply(q1,'5'), q0)
+        .put(PATTERN.apply(q1,'8'), q0)
+
+        .put(PATTERN.apply(q2,'0'), q2)
+        .put(PATTERN.apply(q2,'3'), q2)
+        .put(PATTERN.apply(q2,'6'), q2)
+        .put(PATTERN.apply(q2,'9'), q2)
+
+        .put(PATTERN.apply(q2,'1'), q0)
+        .put(PATTERN.apply(q2,'4'), q0)
+        .put(PATTERN.apply(q2,'7'), q0)
+
+        .put(PATTERN.apply(q2,'2'), q1)
+        .put(PATTERN.apply(q2,'5'), q1)
+        .put(PATTERN.apply(q2,'8'), q1)
+        .build();
+		    return map.get(PATTERN.apply(s,c));
+		};
 		DFA d = new DFA.Builder()
-						.withQ(s1,s2)
-						.withSIGMA('0', '1')
-						.withDelta(delta)
-						.withQ0(s1)
-						.withF(s1)
+						.withQ(q0, q1, q2)
+						.withΣ('0', '1', '2','3','4','5','6','7','8','9')
+						.withδ(δ)
+						.withQ0(q0)
+						.withF(q0)
 						.build();
-		assertTrue(d.accept("010"));
-		assertFalse(d.accept("0101"));
-		d.forceAccept("0101");
+		d.prettyPrint();
 	}
+  @Test
+  public void testMin() {
+    State a = new State.Builder().withId("a").build();
+    State b = new State.Builder().withId("b").build();
+    State c = new State.Builder().withId("c").build();
+    State d = new State.Builder().withId("d").build();
+    State e = new State.Builder().withId("e").build();
+    State f = new State.Builder().withId("f").build();
+    
+
+    BiFunction<State, Character, State> δ =
+        (state, character) -> {
+        ImmutableMap.Builder<String, State> builder = ImmutableMap.builder();
+        Map<String, State> map = builder
+        .put(PATTERN.apply(a,'0'), b)
+        .put(PATTERN.apply(a,'1'), c)
+        .put(PATTERN.apply(b,'0'), a)
+        .put(PATTERN.apply(b,'1'), d)
+        .put(PATTERN.apply(c,'0'), e)
+        .put(PATTERN.apply(c,'1'), f)
+        .put(PATTERN.apply(d,'0'), e)
+        .put(PATTERN.apply(d,'1'), f)
+        .put(PATTERN.apply(e,'0'), e)
+        .put(PATTERN.apply(e,'1'), f)
+        .put(PATTERN.apply(f,'0'), f)
+        .put(PATTERN.apply(f,'1'), f)
+        .build();
+    return map.get(PATTERN.apply(state,character));
+        };
+    DFA dfa = new DFA.Builder()
+            .withQ(a,b,c,d,e,f)
+            .withΣ('0', '1')
+            .withδ(δ)
+            .withQ0(a)
+            .withF(c,d,e)
+            .build()
+            .minimize()
+            ;
+    dfa.prettyPrint();
+  }
 
 }
